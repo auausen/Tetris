2d5ea77e6db1318b4431885106ef1b22e30c2d13
ryosen <mayoibito0317@gmail.com>
2020-11-05 00:46:11 +0900

    Styledtetris

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -9,17 +9,19 @@ import Startbutton from "./StartButton";
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <Startbutton />
-      </aside>
-    </div>
+    <StylleTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <Startbutton />
+        </aside>
+      </StyledTetris>
+    </StylleTetrisWrapper>
   );
 };
 
